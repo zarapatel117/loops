87521e14511ad52af87564c852d5cb6e02e9eed2
zarapatel117 <zarapatel2510@gmail.com~>
2024-12-23 09:49:40 +0530

    revese a string

---
+++ reverse a string.py
@@ -0,0 +1,7 @@
+string = input("enter a word ")
+reversedstring = ("")
+for i in string :
+    reversedstring = i+reversedstring
+print ("orignal string is",string)
+print("reversed string is",reversedstring)
+    
